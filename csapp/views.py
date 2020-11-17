aa368f576e7a3e20cf9d2b256e27bb8ae3f5f8ee
Ilhan Karadeniz <ilhany.karadeniz@gmail.com>
2020-11-18 00:38:46 +0600

    couldnt figure out sessions

--- csapp/views.py
+++ csapp/views.py
@@ -1,26 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.http import HttpResponse
 from django import forms
 
-to_do = ['study german','programming','courses','talking']
+#This is global variable and it can be approached by everybody
+#to_do = ['study german','programming','courses','talking']
 
 class DutyForm(forms.Form):
     your_duty = forms.CharField(label='Your duty', max_length=100)
 
 
 def index(request):
-	return render(request, 'csapp/index.html',context = {
-		"to_do":to_do
+    if 'to_do' not in request.session:
+        #to_do is stored in request.session[],therefore its session variable. Also session is a dictionary
+        request.session['to_do'] = ['hello','HI']
+    
+    return render(request, 'csapp/index.html',context = {
+		"to_do": request.session['to_do']
 		})
 
 def form_ask(request):
     if request.method == "POST":
         myform = DutyForm(request.POST)
-        duty_name = request.POST.get('your_duty')
+        #clean_data wont work before running is_valid()!!
         if myform.is_valid():
-            to_do.append(duty_name)
-            
+            duty_name = myform.cleaned_data["your_duty"]
+            request.session['to_do'].append(duty_name)
+            return HttpResponseRedirect(reverse("csapp:index"))
     else:
         myform = DutyForm()
 
