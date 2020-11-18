ff9f2f55d28b498175f2b0e76e276e95f8121ad1
Ilhan Karadeniz <ilhany.karadeniz@gmail.com>
2020-11-19 05:02:07 +0600

    fixed bugs

--- cs50/urls.py
+++ cs50/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mytasks/',include('csapp.urls')),
+    path('todo/',include('csapp.urls')),
 ]
