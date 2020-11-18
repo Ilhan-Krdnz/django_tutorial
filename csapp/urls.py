3061d036661d49c405091ba1b67f297b394ee9b1
Ilhan Karadeniz <ilhany.karadeniz@gmail.com>
2020-11-18 19:41:30 +0600

    everythings done

--- csapp/urls.py
+++ csapp/urls.py
@@ -5,5 +5,6 @@ app_name = "csapp"
 urlpatterns = [
 	path('',views.index, name='index'),
 	path('form/',views.form_ask,name='form_ask'),
+	path('form_delete/',views.form_delete,name='form_delete'),
 ]
 #path('<str:name>',views.greet, name='greet'),
--- csapp/views.py
+++ csapp/views.py
@@ -10,11 +10,13 @@ from django import forms
 class DutyForm(forms.Form):
     your_duty = forms.CharField(label='Your duty', max_length=100)
 
+class DeleteForm(forms.Form):
+    duty_cancel = forms.CharField(label='cancelled duty', max_length=100)
 
 def index(request):
     if 'to_do' not in request.session:
         #to_do is stored in request.session[],therefore its session variable. Also session is a dictionary
-        request.session['to_do'] = ['hello','HI']
+        request.session['to_do'] = []
     
     return render(request, 'csapp/index.html',context = {
 		"to_do": request.session['to_do']
@@ -25,10 +27,28 @@ def form_ask(request):
         myform = DutyForm(request.POST)
         #clean_data wont work before running is_valid()!!
         if myform.is_valid():
+            request.session.modified = True
+            #mylist = []
             duty_name = myform.cleaned_data["your_duty"]
-            request.session['to_do'] += ['tudy_name'] 
+            #mylist.append(duty_name)
+            request.session['to_do'].append(duty_name) 
             return HttpResponseRedirect(reverse("csapp:index"))
     else:
         myform = DutyForm()
 
-    return render(request, 'csapp/formask.html',context = {"myform":myform})	
+    return render(request, 'csapp/formask.html',context = {"myform":myform})
+
+def form_delete(request):
+    if request.method == 'POST':
+        myform = DeleteForm(request.POST)
+        if myform.is_valid():
+            request.session.modified = True
+            dutyname = myform.cleaned_data['duty_cancel']
+            for i in request.session['to_do']:
+                if i == dutyname:
+                    request.session['to_do'].remove(dutyname)
+
+            return HttpResponseRedirect(reverse("csapp:index"))
+    else:
+        myform = DeleteForm()
+    return render(request, 'csapp/form_delete.html',context = {"myform":myform})	
